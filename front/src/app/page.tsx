0d0648ab89864f8cacc94f7d7f88f6478926a219
M-R-Kuen <157444501+M-R-Kuen@users.noreply.github.com>
2024-09-16 21:13:28 -0300

    update text

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -12,8 +12,8 @@ const LandingPage: React.FC = () => {
 
       {/* Content over the background */}
       <div className="relative z-20 flex flex-col justify-evenly w-full md:mt-[5%]">
-        <div className="flex flex-col items-center justify-center text-white p-8 overflow-hidden lg:flex-row md:items-center md:w-full 2xl:w-3/4 2xl:mx-auto">
-          <div className="flex flex-col items-center md:w-1/2">
+        <div className="flex flex-col items-center justify-center text-white p-8 overflow-hidden  ">
+          <div className="flex flex-col items-center md:w-3/4">
             <Image
               src="/images/Whispering Winds-Logotypewhite.png"
               width={256}
@@ -21,7 +21,7 @@ const LandingPage: React.FC = () => {
               alt="logo"
               className="mx-auto mb-20 mt-10 md:mb-20 md:mt-0"
             />
-            <h1 className="text-4xl text-center text-yellow-600 mt-0 mx-auto w-full gt uppercase md:text-8xl hover:scale-110">
+            <h1 className="text-4xl text-center  text-yellow-600 mt-0 mx-auto w-full glatic tracking-tighter uppercase  lg:text-8xl hover:scale-110">
               Empowering adventurous souls
             </h1>
             <h3 className="sfThin text-2xl md:text-4xl mt-10">
