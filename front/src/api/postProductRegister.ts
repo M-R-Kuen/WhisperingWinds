1b9bb430842977cee98c268efb5033868741679f
M-R-Kuen <157444501+M-R-Kuen@users.noreply.github.com>
2024-09-29 21:18:47 -0300

    update api

--- front/src/api/postProductRegister.ts
+++ front/src/api/postProductRegister.ts
@@ -1,15 +1,29 @@
 import { IProductRegister } from "@/interfaces/productRegister";
 import axios from "axios";
 
+const apiUrl = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000";
 export const postProductRegister = async (data: IProductRegister) => {
   try {
-    const response = await axios.post(
-      "http://localhost:3000/register-product",
-      data
-    );
+    const response = await axios.post(`${apiUrl}/register-product`, data);
     console.log(response.data);
     return response.data;
   } catch (error) {
     console.error(error);
   }
 };
+
+// import { IProductRegister } from "@/interfaces/productRegister";
+// import axios from "axios";
+
+// export const postProductRegister = async (data: IProductRegister) => {
+//   try {
+//     const response = await axios.post(
+//       "http://localhost:3000/register-product",
+//       data
+//     );
+//     console.log(response.data);
+//     return response.data;
+//   } catch (error) {
+//     console.error(error);
+//   }
+// };
