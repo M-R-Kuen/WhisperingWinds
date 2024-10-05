3c4ceb39faa52e2818947380c76055ef2cd4a27e
M-R-Kuen <157444501+M-R-Kuen@users.noreply.github.com>
2024-10-04 21:18:31 -0300

    update form

--- front/src/components/UI/ReusableFormComponent/ButtonForm/ButtonForm.tsx
+++ front/src/components/UI/ReusableFormComponent/ButtonForm/ButtonForm.tsx
@@ -5,7 +5,7 @@ const ButtonForm: React.FC<IButtonForm> = ({ name, type }) => {
   return (
     <div className="flex flex-col mt-10 w-[240px]">
       <button
-        className=" items-center text-center justify-center  py-[8px] px-[10px]  text-white  rounded-lg hover:shadow-lg hover:shadow-white cyGroteskLight uppercase"
+        className=" items-center text-center justify-center  py-[9px] px-[10px]  text-white  rounded-lg border border-white cyGroteskLight uppercase"
         type={type}
       >
         {name}
